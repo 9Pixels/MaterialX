3ce5cc44555bbf0bb4346c12240f537cd6c6f79f
Robciuc Ioan <ionut@9pixels.co>
2015-06-11 17:42:03 +0300

    Issue (Font hardcoded) fixed
    Issue (Font hardcoded) fixed.

--- functions.php
+++ functions.php
@@ -6,16 +6,23 @@
 if( !function_exists( 'materialx_theme_setup' ) ) {
 	add_action( 'after_setup_theme', 'materialx_theme_setup' );
 	function materialx_theme_setup() {
-		load_theme_textdomain( 'accountant', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'materialx', get_template_directory() . '/languages' );
 	}
 }
+
 /**
  *	Enqueue Styles
  */
 if( !function_exists( 'materialx_enqueue_styles' ) ) {
 	add_action( 'wp_enqueue_scripts', 'materialx_enqueue_styles' );
 	function materialx_enqueue_styles() {
-		wp_enqueue_style( 'font-family-roboto', 'http://fonts.googleapis.com/css?family=Roboto:200,300,400,700,600,400italic,700italic,300italic' );
+
+		$query_args = array(
+			'family'	=> 'Roboto:200,300,400,700,600,400italic,700italic,300italic'
+		);
+		wp_register_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
+
 	    wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
+	    wp_enqueue_style( 'google_fonts' );
 	}
 }
