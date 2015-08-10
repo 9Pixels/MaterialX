a5843f4ad679c980614e16b33c8c5e9f64c3cc2d
Robciuc Ioan <ionut@9pixels.co>
2015-08-10 23:44:59 +0300

    Issue (Tema nu are escape la informatiile scoase in front-end, cel care a facut review-ul est eun pic mai exigent, a zis sa faci esc_html si la the_title() :)) fixed.

--- single.php
+++ single.php
@@ -10,25 +10,25 @@ get_header(); ?>
 			<?php 
 			$metro_creativex_template_url = get_template_directory_uri();
 			if (has_post_format( 'aside' )) {
-				echo '<h1 class="insidepost bg-design" style="background-image:url('.$metro_creativex_template_url.'/images/pt_aside.png);">'.get_the_title().'</h1>';
+				echo '<h1 class="insidepost bg-design" style="background-image:url('.$metro_creativex_template_url.'/images/pt_aside.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'audio' )) {
-				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('.$metro_creativex_template_url.'/images/pt_audio.png);">'.get_the_title().'</h1>';
+				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('.$metro_creativex_template_url.'/images/pt_audio.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'chat' )) {
-				echo '<h1 class="insidepost bg-stuff" style="background-image:url('.$metro_creativex_template_url.'/images/pt_chat.png);">'.get_the_title().'</h1>';
+				echo '<h1 class="insidepost bg-stuff" style="background-image:url('.$metro_creativex_template_url.'/images/pt_chat.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'gallery' )) {
-				echo '<h1 class="insidepost bg-responsive" style="background-image:url('.$metro_creativex_template_url.'/images/pt_gallery.png);">'.get_the_title().'</h1>';
+				echo '<h1 class="insidepost bg-responsive" style="background-image:url('.$metro_creativex_template_url.'/images/pt_gallery.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'image' )) {
-				echo '<h1 class="insidepost bg-responsive" style="background-image:url('.$metro_creativex_template_url.'/images/pt_image.png);">'.get_the_title().'</h1>';
+				echo '<h1 class="insidepost bg-responsive" style="background-image:url('.$metro_creativex_template_url.'/images/pt_image.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'link' )) {
-				echo '<h1 class="insidepost bg-web" style="background-image:url('.$metro_creativex_template_url.'/images/pt_link.png);">'.get_the_title().'</h1>';
+				echo '<h1 class="insidepost bg-web" style="background-image:url('.$metro_creativex_template_url.'/images/pt_link.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'quote' )) {
-				echo '<h1 class="insidepost bg-web" style="background-image:url('.$metro_creativex_template_url.'/images/pt_quote.png);">'.get_the_title().'</h1>';
+				echo '<h1 class="insidepost bg-web" style="background-image:url('.$metro_creativex_template_url.'/images/pt_quote.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'status' )) {
-				echo '<h1 class="insidepost bg-web" style="background-image:url('.$metro_creativex_template_url.'/images/pt_status.png);">'.get_the_title().'</h1>';
+				echo '<h1 class="insidepost bg-web" style="background-image:url('.$metro_creativex_template_url.'/images/pt_status.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'video' )) {
-				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('.$metro_creativex_template_url.'/images/pt_video.png);">'.get_the_title().'</h1>';
+				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('.$metro_creativex_template_url.'/images/pt_video.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} else {
-				echo '<h1 class="insidepost bg-stuff" style="background-image:url('.$metro_creativex_template_url.'/images/pt_standard.png);">'.get_the_title().'</h1>';
+				echo '<h1 class="insidepost bg-stuff" style="background-image:url('.$metro_creativex_template_url.'/images/pt_standard.png);">'. esc_html( get_the_title() ) .'</h1>';
 			}
 			?>
 		<div class="insidepost_date"><?php echo get_the_date(); ?> - <?php the_category(', ') ?></div>
@@ -39,7 +39,7 @@ get_header(); ?>
 				$metro_creativex_posttitle = get_the_title();
 				$metro_creativex_feat_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
 				if(isset($metro_creativex_feat_image[0])):
-					echo '<div class="post_img"><img src='.$metro_creativex_feat_image[0].' alt="'.$metro_creativex_posttitle.'"></div>';
+					echo '<div class="post_img"><img src='.$metro_creativex_feat_image[0].' alt="'. esc_html( $metro_creativex_posttitle ) .'"></div>';
 				endif;	
 			
 				the_content(); 
