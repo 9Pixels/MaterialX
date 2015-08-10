293cf92c69158f029be600cbf0963a81966296da
Robciuc Ioan <ionut@9pixels.co>
2015-08-10 23:52:50 +0300

    Issue (Variables are prefixed with {parent_theme_slug_name} instead of {child_theme_slug_name}. Please fix this.) fixed.

--- single.php
+++ single.php
@@ -8,27 +8,27 @@
 get_header(); ?>
 <?php  while ( have_posts() ) : the_post(); ?>
 			<?php 
-			$metro_creativex_template_url = get_template_directory_uri();
+			$materialx_template_url = get_template_directory_uri();
 			if (has_post_format( 'aside' )) {
-				echo '<h1 class="insidepost bg-design" style="background-image:url('.$metro_creativex_template_url.'/images/pt_aside.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-design" style="background-image:url('.$materialx_template_url.'/images/pt_aside.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'audio' )) {
-				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('.$metro_creativex_template_url.'/images/pt_audio.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('.$materialx_template_url.'/images/pt_audio.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'chat' )) {
-				echo '<h1 class="insidepost bg-stuff" style="background-image:url('.$metro_creativex_template_url.'/images/pt_chat.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-stuff" style="background-image:url('.$materialx_template_url.'/images/pt_chat.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'gallery' )) {
-				echo '<h1 class="insidepost bg-responsive" style="background-image:url('.$metro_creativex_template_url.'/images/pt_gallery.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-responsive" style="background-image:url('.$materialx_template_url.'/images/pt_gallery.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'image' )) {
-				echo '<h1 class="insidepost bg-responsive" style="background-image:url('.$metro_creativex_template_url.'/images/pt_image.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-responsive" style="background-image:url('.$materialx_template_url.'/images/pt_image.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'link' )) {
-				echo '<h1 class="insidepost bg-web" style="background-image:url('.$metro_creativex_template_url.'/images/pt_link.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-web" style="background-image:url('.$materialx_template_url.'/images/pt_link.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'quote' )) {
-				echo '<h1 class="insidepost bg-web" style="background-image:url('.$metro_creativex_template_url.'/images/pt_quote.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-web" style="background-image:url('.$materialx_template_url.'/images/pt_quote.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'status' )) {
-				echo '<h1 class="insidepost bg-web" style="background-image:url('.$metro_creativex_template_url.'/images/pt_status.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-web" style="background-image:url('.$materialx_template_url.'/images/pt_status.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'video' )) {
-				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('.$metro_creativex_template_url.'/images/pt_video.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('.$materialx_template_url.'/images/pt_video.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} else {
-				echo '<h1 class="insidepost bg-stuff" style="background-image:url('.$metro_creativex_template_url.'/images/pt_standard.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-stuff" style="background-image:url('.$materialx_template_url.'/images/pt_standard.png);">'. esc_html( get_the_title() ) .'</h1>';
 			}
 			?>
 		<div class="insidepost_date"><?php echo get_the_date(); ?> - <?php the_category(', ') ?></div>
@@ -36,10 +36,10 @@ get_header(); ?>
 	<div id="content">
 		<div <?php post_class(); ?>>
 			<?php 
-				$metro_creativex_posttitle = get_the_title();
-				$metro_creativex_feat_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-				if(isset($metro_creativex_feat_image[0])):
-					echo '<div class="post_img"><img src='. esc_url( $metro_creativex_feat_image[0] ) .' alt="'. esc_html( $metro_creativex_posttitle ) .'"></div>';
+				$materialx_posttitle = get_the_title();
+				$materialx_feat_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+				if(isset($materialx_feat_image[0])):
+					echo '<div class="post_img"><img src='. esc_url( $materialx_feat_image[0] ) .' alt="'. esc_html( $materialx_posttitle ) .'"></div>';
 				endif;	
 			
 				the_content(); 
