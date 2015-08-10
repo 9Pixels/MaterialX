66f4801688e1c4100aedf86ceea7621eb01fcd17
Robciuc Ioan <ionut@9pixels.co>
2015-08-10 22:53:59 +0300

    I changed the function from home_url to get_site_url.

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 		<div class="clearfix"></div>
 		<footer>
-			<span class="alignleft">&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a><span> &mdash; <?php printf( __( 'Powered by <a href="%s">WordPress</a>', 'materialx'), 'http://www.wordpress.org' ); ?></span></span><br\>			
+			<span class="alignleft">&copy; <?php echo date("Y") ?> <a href="<?php echo get_site_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a><span> &mdash; <?php printf( __( 'Powered by <a href="%s">WordPress</a>', 'materialx'), 'http://www.wordpress.org' ); ?></span></span><br\>			
 			</footer>
 	<?php wp_footer(); ?>
 	</body>
