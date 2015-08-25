8970ce03a461bf5ffef094b870a3c33189ce69a7
Robciuc Ioan <ionut@9pixels.co>
2015-08-26 01:28:24 +0300

    Issue (fa te rog escape la get_template_directory_uri() in toata tema) fixed.

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
 if( !function_exists( 'materialx_theme_setup' ) ) {
 	add_action( 'after_setup_theme', 'materialx_theme_setup' );
 	function materialx_theme_setup() {
-		load_theme_textdomain( 'materialx', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'materialx', esc_url( get_template_directory() ) . '/languages' );
 	}
 }
 
@@ -22,7 +22,7 @@ if( !function_exists( 'materialx_enqueue_styles' ) ) {
 		);
 		wp_register_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
 
-	    wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
+	    wp_enqueue_style( 'style', esc_url( get_template_directory_uri() ) . '/style.css' );
 	    wp_enqueue_style( 'google_fonts' );
 	}
 }
--- single.php
+++ single.php
@@ -8,27 +8,27 @@
 get_header(); ?>
 <?php  while ( have_posts() ) : the_post(); ?>
 			<?php 
-			$materialx_template_url = get_template_directory_uri();
+			$materialx_template_url  = get_template_directory_uri();
 			if (has_post_format( 'aside' )) {
-				echo '<h1 class="insidepost bg-design" style="background-image:url('.$materialx_template_url.'/images/pt_aside.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-design" style="background-image:url('. esc_url( $materialx_template_url ) .'/images/pt_aside.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'audio' )) {
-				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('.$materialx_template_url.'/images/pt_audio.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('. esc_url( $materialx_template_url ) .'/images/pt_audio.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'chat' )) {
-				echo '<h1 class="insidepost bg-stuff" style="background-image:url('.$materialx_template_url.'/images/pt_chat.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-stuff" style="background-image:url('. esc_url( $materialx_template_url ) .'/images/pt_chat.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'gallery' )) {
-				echo '<h1 class="insidepost bg-responsive" style="background-image:url('.$materialx_template_url.'/images/pt_gallery.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-responsive" style="background-image:url('. esc_url( $materialx_template_url ) .'/images/pt_gallery.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'image' )) {
-				echo '<h1 class="insidepost bg-responsive" style="background-image:url('.$materialx_template_url.'/images/pt_image.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-responsive" style="background-image:url('. esc_url( $materialx_template_url ) .'/images/pt_image.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'link' )) {
-				echo '<h1 class="insidepost bg-web" style="background-image:url('.$materialx_template_url.'/images/pt_link.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-web" style="background-image:url('. esc_url( $materialx_template_url ) .'/images/pt_link.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'quote' )) {
-				echo '<h1 class="insidepost bg-web" style="background-image:url('.$materialx_template_url.'/images/pt_quote.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-web" style="background-image:url('. esc_url( $materialx_template_url ) .'/images/pt_quote.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'status' )) {
-				echo '<h1 class="insidepost bg-web" style="background-image:url('.$materialx_template_url.'/images/pt_status.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-web" style="background-image:url('. esc_url( $materialx_template_url ) .'/images/pt_status.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} elseif (has_post_format( 'video' )) {
-				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('.$materialx_template_url.'/images/pt_video.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-wordpress" style="background-image:url('. esc_url( $materialx_template_url ) .'/images/pt_video.png);">'. esc_html( get_the_title() ) .'</h1>';
 			} else {
-				echo '<h1 class="insidepost bg-stuff" style="background-image:url('.$materialx_template_url.'/images/pt_standard.png);">'. esc_html( get_the_title() ) .'</h1>';
+				echo '<h1 class="insidepost bg-stuff" style="background-image:url('. esc_url( $materialx_template_url ) .'/images/pt_standard.png);">'. esc_html( get_the_title() ) .'</h1>';
 			}
 			?>
 		<div class="insidepost_date"><?php echo get_the_date(); ?> - <?php the_category(', ') ?></div>
