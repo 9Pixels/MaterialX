1888eab08774dbfc3e1d3498da5bb3f447ca9d3c
Robciuc Ioan <ionut@9pixels.co>
2015-08-26 01:44:04 +0300

    Issue (Tot ce este bloginfo si get_option fa esc_html sau esc_url, in functie de ce returneaza) fixed.

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 		<div class="clearfix"></div>
 		<footer>
-			<span class="alignleft">&copy; <?php echo date("Y") ?> <a href="<?php echo esc_url( get_site_url() ); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a><span> &mdash; <?php printf( __( 'Powered by <a href="%s">WordPress</a>', 'materialx'), 'http://www.wordpress.org' ); ?></span></span><br\>			
+			<span class="alignleft">&copy; <?php echo date("Y") ?> <a href="<?php echo esc_url( get_site_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><span> &mdash; <?php printf( __( 'Powered by <a href="%s">WordPress</a>', 'materialx'), 'http://www.wordpress.org' ); ?></span></span><br\>			
 			</footer>
 	<?php wp_footer(); ?>
 	</body>
